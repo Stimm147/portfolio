a2a87c0dcdf331edee3bf38584b00dd2dc932b50
Stimm147 <michalczewski0@gmail.com>
2025-07-13 01:52:09 +0200

    urls

--- rxconfig.py
+++ rxconfig.py
@@ -2,10 +2,10 @@ import reflex as rx
 
 config = rx.Config(
     app_name="portfolio",
-    base_path="/portfolio",  # ważne, zostaw to
+    base_path="/portfolio",  # konieczne, musi być dokładnie "/portfolio"
     deploy_url="https://stimm147.github.io/portfolio/",
-    vite_config={  # <<< tu
-        "base": "/portfolio/",  # <<< kluczowe
+    vite_config={
+        "base": "/portfolio/",  # kluczowe - Vite musi mieć bazę pod subfolder
     },
     plugins=[
         rx.plugins.SitemapPlugin(),
