26661cd19b78472f869aa907e989030a570ce4c9
Stimm147 <michalczewski0@gmail.com>
2025-07-13 00:15:55 +0200

    main website update

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -1,7 +1,6 @@
 import reflex as rx
 from .pages.homepage import homepage
 from rxconfig import config
-from .pages.about import about_page
 
 
 def index() -> rx.Component:
@@ -9,14 +8,10 @@ def index() -> rx.Component:
         homepage(),
     )
 
+
 app = rx.App()
 
 app.add_page(
     index,
     route="/",
-)
-
-app.add_page(
-    about_page,
-    route="/about",
 )
