e16f101ffc7c3832219982c2caf1062b716da8f5
Stimm147 <michalczewski0@gmail.com>
2025-07-13 00:45:50 +0200

    url fix

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="portfolio",
+    frontend_path="/portfolio/",
+    deploy_url="https://stimm147.github.io/portfolio/",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
