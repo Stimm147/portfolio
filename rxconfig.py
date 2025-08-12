57a9bf0da1be4c247f4957be56af3152e290c8e4
stimm147 <michalczewski0@gmail.com>
2025-08-12 21:56:16 +0200

    cors modified

--- rxconfig.py
+++ rxconfig.py
@@ -2,12 +2,6 @@ import os
 import reflex as rx
 
 api_url = os.getenv("API_URL", "http://127.0.0.1:8000")
-frontend_url = os.getenv("RAILWAY_PUBLIC_DOMAIN", "localhost")
-full_frontend_url = (
-    f"https://{frontend_url}"
-    if frontend_url != "localhost"
-    else "http://localhost:3000"
-)
 
 config = rx.Config(
     app_name="portfolio",
@@ -18,8 +12,6 @@ config = rx.Config(
     api_url=api_url,
     cors_allowed_origins=[
         "http://localhost:3000",
-        full_frontend_url,
+        "https://portfolio-production-3beb.up.railway.app/",
     ],
-    frontend_host="0.0.0.0",
-    backend_host="0.0.0.0",
 )
