94021828777ab313a66b90b83d60a77d94b283f3
stimm147 <michalczewski0@gmail.com>
2025-08-12 20:56:38 +0200

    port updated

--- rxconfig.py
+++ rxconfig.py
@@ -1,11 +1,18 @@
 import os
 import reflex as rx
 
+api_url = os.getenv("API_URL", "http://127.0.0.1:8000")
+frontend_url = os.getenv("RAILWAY_PUBLIC_DOMAIN", "localhost")
+
 config = rx.Config(
     app_name="portfolio",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
     ],
-    api_url=os.environ.get("API_URL") or "http://localhost:8000",
+    api_url=api_url,
+    cors_allowed_origins=[
+        "http://localhost:3000",
+        f"https://{frontend_url}",  # Dynamiczne dodawanie domeny z Railway
+    ],
 )
