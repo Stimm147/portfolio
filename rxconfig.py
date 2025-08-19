a43bf38cd8bb53d1ff5c3440960c049f5d6b3a2f
stimm147 <michalczewski0@gmail.com>
2025-08-19 16:33:21 +0200

    optimization 1

--- rxconfig.py
+++ rxconfig.py
@@ -1,7 +1,12 @@
+import gc
 import os
 import reflex as rx
 
 api_url = os.getenv("API_URL", "http://127.0.0.1:8000")
+
+if os.getenv("ENV") == "prod":
+    # Włącz aggressive garbage collection
+    gc.set_threshold(100, 10, 10)
 
 config = rx.Config(
     app_name="portfolio",
@@ -14,4 +19,6 @@ config = rx.Config(
         "http://localhost:3000",
         "https://michal-sztymelski-portfolio.up.railway.app",
     ],
+    compile=True,
+    telemetry_enabled=False,
 )
